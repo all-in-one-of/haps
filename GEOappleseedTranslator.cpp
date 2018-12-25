2ce4689a266f6f0da0892ef7712c1f96efa99243
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2018-12-25 23:50:21 +0100

    Appleseed translator based on GT library.

---
+++ GEOappleseedTranslator.cpp
@@ -0,0 +1,88 @@
+
+#include <UT/UT_DSOVersion.h>
+#include <GU/GU_Detail.h>
+#include <GU/GU_PrimVolume.h>
+#include <GEO/GEO_AttributeHandle.h>
+#include <GEO/GEO_IOTranslator.h>
+#include <SOP/SOP_Node.h>
+#include <UT/UT_Assert.h>
+#include <UT/UT_IOTable.h>
+#include <iostream>
+#include <fstream>
+#include "binarymesh.h"
+#include <stdio.h>
+
+
+namespace HDK_HAPS 
+{
+class GEO_HAPSIOTranslator : public GEO_IOTranslator
+{
+public:
+             GEO_HAPSIOTranslator() {}
+    virtual ~GEO_HAPSIOTranslator() {}
+    virtual GEO_IOTranslator *duplicate() const;
+    virtual const char *formatName() const;
+    virtual int         checkExtension(const char *name);
+    virtual int         checkMagicNumber(unsigned magic);
+    virtual GA_Detail::IOStatus fileLoad(GEO_Detail *gdp, UT_IStream &is, bool ate_magic);
+    virtual GA_Detail::IOStatus fileSave(const GEO_Detail *gdp, std::ostream &os);
+    virtual GA_Detail::IOStatus fileSaveToFile(const GEO_Detail *gdp, const char *fname);
+};
+}
+
+using namespace HDK_HAPS;
+
+GEO_IOTranslator *
+GEO_HAPSIOTranslator::duplicate() const
+{
+    return new GEO_HAPSIOTranslator();
+}
+const char *
+GEO_HAPSIOTranslator::formatName() const
+{
+    return "Appleseed binarymesh";
+}
+int
+GEO_HAPSIOTranslator::checkExtension(const char *name) 
+{
+    UT_String           sname(name);
+    if (sname.fileExtension() && !strcmp(sname.fileExtension(), ".binarymesh"))
+        return true;
+    return false;
+}
+int
+GEO_HAPSIOTranslator::checkMagicNumber(unsigned magic)
+{
+    return 0;
+}
+GA_Detail::IOStatus
+GEO_HAPSIOTranslator::fileLoad(GEO_Detail *gdp, UT_IStream &is, bool ate_magic)
+{
+    std::cerr << "Error: Not impemented yet.\n";
+    return false;
+
+}
+GA_Detail::IOStatus
+GEO_HAPSIOTranslator::fileSave(const GEO_Detail *gdp, std::ostream &os)
+{
+    return false;
+}
+GA_Detail::IOStatus
+GEO_HAPSIOTranslator::fileSaveToFile(const GEO_Detail *gdp, const char *fname)
+{
+    if (!fname)
+        return false;
+    if(save_binarymesh(gdp, fname))
+        return  GA_Detail::IOStatus(true);
+
+    return false;
+}
+void
+newGeometryIO(void *)
+{
+    GU_Detail::registerIOTranslator(new GEO_HAPSIOTranslator());
+    UT_ExtensionList            *geoextension;
+    geoextension = UTgetGeoExtensions();
+    if (!geoextension->findExtension("binarymesh"))
+        geoextension->addExtension("binarymesh");
+}
---
+++ binarymesh.h
@@ -0,0 +1,166 @@
+#include <GT/GT_GEODetail.h>
+#include <GT/GT_PrimPolygonMesh.h>
+#include <GT/GT_DAConstantValue.h>
+
+namespace HDK_HAPS
+{
+
+constexpr ushort BINARYMESH_VERSION = 1;
+
+void write_header(std::fstream &fs) {
+    const char   header[]   = {'B', 'I', 'N', 'A', 'R', 'Y', 'M', 'E', 'S', 'H'};
+    const ushort version    = BINARYMESH_VERSION;
+    fs.write(header, 10);
+    fs.write((char*)&version, 2);
+}
+
+void write_group(std::fstream & fs, const char *group) {
+    const ushort lenght = strlen(group);
+    fs.write((char*)&lenght, sizeof(ushort));
+    fs.write(group, lenght*sizeof(char));
+    
+}
+
+void write_doubles_array(std::fstream & fs, GT_DataArrayHandle & buffer, 
+        const GT_DataArrayHandle & handle) {
+    const uint   entries  = handle->entries();
+    const size_t bytesize = entries*handle->getTupleSize()*sizeof(fpreal64);
+    const fpreal64 * ptr = handle->getF64Array(buffer);
+    fs.write((char*)&entries, sizeof(uint));
+    fs.write((char*)ptr, bytesize);
+}
+
+void write_material_slots(std::fstream &fs, 
+        const std::vector<std::string> &materials){
+
+    const ushort slots = materials.size();
+    fs.write((char*)&slots, sizeof(ushort));
+
+    for(const auto & item: materials) {
+        const char * matname = item.c_str();
+        const ushort length = strlen(matname);
+        fs.write((char*)&length, sizeof(ushort));
+        fs.write((char*)matname, length*sizeof(char));   
+    }
+}
+
+
+
+int save_binarymesh(const GEO_Detail *detail, const char *filename)
+{
+    // Prepere for tesselation library GT_
+    GU_Detail geometry;
+    geometry.copy(*detail); // do we really need to copy?
+    GU_DetailHandle detailhandle;
+    detailhandle.allocateAndSet(&geometry, false);
+    GU_ConstDetailHandle constdetailh(detailhandle);
+    auto polymeshhandle   = GT_GEODetail::makePolygonMesh(constdetailh);
+    auto polymesh         = UTverify_cast<const GT_PrimPolygonMesh *>(polymeshhandle.get());
+    auto convexmeshhandle = polymesh->convex();
+    auto tesselated       = UTverify_cast<const GT_PrimPolygonMesh *>(convexmeshhandle.get());
+   
+    if (!tesselated) {
+        std::cerr << "Cant create tesselated geometry. \n";
+        return 0;
+    }
+
+    // filestream
+    auto binarymesh = std::fstream(filename, 
+        std::ios::out | std::ios::binary);
+
+    // header
+    write_header(binarymesh);
+
+     // part name
+    const char* group = "default";
+    write_group(binarymesh, group);
+
+    // points
+    GT_Owner point_owner = GT_OWNER_POINT;
+    GT_DataArrayHandle positionhandle = tesselated->findAttribute(
+        UT_StringRef("P"), point_owner, 0);
+    GT_DataArrayHandle buffer;
+    write_doubles_array(binarymesh, buffer, positionhandle);
+
+    
+    // normals
+    const GT_PrimPolygonMesh * tmp = tesselated;
+    tesselated = tesselated->createPointNormalsIfMissing();
+    if (!tesselated) {
+        tesselated = tmp;
+    }
+
+    GT_Owner vertex_owner = GT_OWNER_VERTEX;
+    auto normalhandle = tesselated->findAttribute(UT_StringRef("N"), vertex_owner, 0);
+    if (!normalhandle) {
+        normalhandle = tesselated->findAttribute(UT_StringRef("N"), point_owner, 0);
+    }
+    write_doubles_array(binarymesh, buffer, normalhandle);
+
+    // FIXME: atm uvs have to have the same type as normals (both vertex or points)
+    // FIXME: uvs have to have tupleSize() == 2
+    auto uvhandle = tesselated->findAttribute(UT_StringRef("uv"), vertex_owner, 0);
+    if (!uvhandle) {
+        uvhandle = tesselated->findAttribute(UT_StringRef("uv"), point_owner, 0);
+        if (!uvhandle) {
+            uvhandle = GT_DataArrayHandle(new GT_RealConstant(normalhandle->entries(), 0.0, 2));
+        }
+    }
+    write_doubles_array(binarymesh, buffer, uvhandle);
+
+    // Material
+    ushort mindex = 0;
+    std::vector<std::string> materials;
+    GT_Owner prim_owner = GT_OWNER_PRIMITIVE;
+    auto materialhandle = tesselated->findAttribute(
+        UT_StringRef("shop_materialpath"), prim_owner, 0);
+
+    if (materialhandle) {
+        UT_StringArray shops_strings;
+        materialhandle->getStrings(shops_strings);
+        for(size_t m=0; m<shops_strings.entries(); ++m)
+            materials.push_back(std::string(shops_strings[m]));
+
+    } else {
+        materials.push_back(std::string("default"));
+    }
+
+    write_material_slots(binarymesh, materials);
+
+    // faces:
+    const uint nfaces = tesselated->getFaceCount();
+    binarymesh.write((char*)&nfaces, sizeof(uint));
+    GT_DataArrayHandle verts; GT_DataArrayHandle uniform_indexing;
+    GT_DataArrayHandle vertex_indexing; GT_DataArrayHandle vert_info;
+    GT_DataArrayHandle prim_info;
+    
+    tesselated->getConvexArrays(verts, uniform_indexing, 
+        vertex_indexing, vert_info, prim_info);
+
+    uint vidx = 0;
+    GT_DataArrayHandle vperface = tesselated->getFaceCounts();
+    for(size_t f=0; f<nfaces; ++f) {
+        const ushort nv = vperface->getI16(GT_Offset(f));
+        binarymesh.write((char*)&nv, sizeof(ushort));
+        const GT_Offset voff = tesselated->getVertexOffset(GT_Offset(f));
+        for (ushort v=0; v<nv; ++v) {
+            const uint vp = verts->getI32(GT_Offset(voff+v));
+            //  is it really the case?
+            const uint vi = vertex_indexing->getI32(vidx);
+            binarymesh.write((char*)&vp, sizeof(uint));
+            binarymesh.write((char*)&vi, sizeof(uint));
+            binarymesh.write((char*)&vi, sizeof(uint));
+            vidx++;
+        }
+        if (materialhandle) {
+            mindex = materialhandle->getStringIndex(f);
+        }
+            binarymesh.write((char*)&mindex, sizeof(ushort));
+    }
+
+
+    binarymesh.close();
+    return 1;
+}
+
+} // end of namespace HDK_HAPS
