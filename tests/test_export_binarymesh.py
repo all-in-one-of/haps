04bbd1bd6bfe29907adb611410d78248a1cefe53
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2019-02-07 15:52:49 +0100

    Fix for binary mesh export python test.

--- tests/test_export_binarymesh.py
+++ tests/test_export_binarymesh.py
@@ -4,26 +4,26 @@ import os
 import unittest
 
 class BinaryMeshTestCase(unittest.TestCase):
-    houdini_available = False
     binarymesh = None
+
     def setUp(self):
         try:
             import hou
-            self.box = hou.node("/obj/box/OUT")
-            self.houdini_available = True
         except:
             self.skipTest("Can't proceed without hou module.")
 
+        geo  = hou.node('/obj').createNode("geo")
+        self.box = geo.createNode('box')
+
     def tearDown(self):
-        if not self.houdini_available:
-            return
-        if os.path.exists(self.binarymesh.name):
-            os.unlink(self.binarymesh.name)
+        if os.path.exists(self.binarymesh):
+            os.unlink(self.binarymesh)
 
     def test_exportBinaryMesh(self):
         clockstart = time.time()
-        self.binarymesh = tempfile.NamedTemporaryFile(False)
-        self.box.geometry.saveToFile(self.binarymesh+'.binarymesh')
+        self.binarymesh = os.path.join(tempfile.gettempdir(), 
+            tempfile.gettempprefix() + '.binarymesh')
+        self.box.geometry().saveToFile(self.binarymesh)
         print('Export time: %g seconds' % (time.time() - clockstart))
-        self.assertTrue(os.path.exists(self.binarymesh.name))
+        self.assertTrue(os.path.exists(self.binarymesh))
 
