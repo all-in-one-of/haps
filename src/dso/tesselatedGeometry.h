277e8849b315527241389521e8dd7e2149d0a4bd
Szymon Kapeniak <szymon.kapeniak@gmail.com>
2019-02-08 16:27:44 +0100

    Adding explicit keyword and default destructor.

--- src/dso/tesselatedGeometry.h
+++ src/dso/tesselatedGeometry.h
@@ -29,28 +29,37 @@ class TesselatedGeometry
 {
 public:
     // We save copy of GEO_Detail and tesselate it on the fly
-    TesselatedGeometry(const GEO_Detail *);
+    explicit TesselatedGeometry(const GEO_Detail *);
+    ~TesselatedGeometry() = default;
+
     // Find attribute by name, store attribute type in GT_Owner
     GT_DataArrayHandle find_attribute(const char *, GT_Owner attribute_owner=GT_OWNER_INVALID, 
     		const GT_Primitive * prim=nullptr) const; 
+
     // Save attribute to the provided stream
     size_t save_attribute(std::ostream &, const GT_DataArrayHandle &);
+
     // Get our copy of tesselated geometry
     const GT_PrimPolygonMesh * mesh() const { return tesselated; }
+
     // Are we valid? 
     bool                    isValid() const { return valid; }
     
 private:
     // Tesselate routine 
     bool tesselate(const bool compute_normals=true, const bool vertex_normals=false);
+
     GU_DetailHandle      detailhandle;
     GU_ConstDetailHandle consthandle;
     GT_PrimitiveHandle   geometry;
     GU_Detail            gdpcopy;
+
     const GT_PrimPolygonMesh * 
                     tesselated  = nullptr;
+
     // we use to to copy attributes to stream
     std::unique_ptr<T[]> buffer = nullptr;
+
     // current buffer size 
     size_t    buffersize  = 0;
     bool      valid       = false;
